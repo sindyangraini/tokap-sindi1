367705cab99fd784c8050b43b37f53a9b212e41f
sindyangraini <sindyangrraini214@gmail.com>
2019-02-11 12:15:23 +0700

    vid 8

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -12,34 +12,46 @@
 				{{ csrf_field() }}
 				<div class="form-group form-label-group">
 					<input type="text" name="name"
-					class="form-control"
-					value="{{ old('name',$dt->name) }}"
+					class="form-control {{$errors->has('name')?'is-invalid':''}}"
+					value="{{ old('name',$dt->name)}}"
 					id="iName" placeholder="Name" required>
 					<label for="iName">Name</label>
+					@if($errors->has('name'))
+					<div class="invalid-feedback">{{$errors->first('name')}}</div>
+					@endif
 				</div>
 
 			<div class="form-group form-label-group">
 				<input type="text" name="email"
-				class="form-control"
+				class="form-control {{$errors->has('email')?'is-invalid':''}}"
 				value="{{ old('email',$dt->email) }}"
 				id="iEmail" placeholder="Email" required>
 				<label for="iEmail">Email</label>
+				@if($errors->has('email'))
+					<div class="invalid-feedback">{{$errors->first('email')}}</div>
+					@endif
 			</div>	
 
 			<div class="form-group form-label-group">
 				<input type="password" name="password"
-				class="form-control"
+				class="form-control {{$errors->has('password')?'is-invalid':''}}"
 				id="iPassword" placeholder="Password">
 				<label for="iPassword">Password</label>
+				@if($errors->has('password'))
+					<div class="invalid-feedback">{{$errors->first('password')}}</div>
+					@endif
 				<div class="form-text text-mmuted">
 					<small>Kosongkan Password apabila tidak diubah.</small>
 				</div>
 			</div>
 			<div class="form-group form-label-group">
-				<input type="password" name="repassword"
-				class="form-control"
+		 		<input type="password" name="repassword"
+				class="form-control {{$errors->has('repassword')?'is-invalid':''}}"
 				id="iRePassword" placeholder="Re Password">
-				<label for="iRePassword">Re Password</label>				
+				<label for="iRePassword">Re Password</label>
+				@if($errors->has('repassword'))
+					<div class="invalid-feedback">{{$errors->first('repassword')}}</div>
+					@endif				
 			</div>
 
 		</div>
@@ -47,7 +59,7 @@
 			<button type="submit" class="btn btn-primary shadow-sm">Update</button>
 		</div>
 		</div>
-
+ 
 	</form>
 	</div>
 </div>
