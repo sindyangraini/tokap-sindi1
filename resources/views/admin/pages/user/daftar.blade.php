688ca7c02be200de89c3c034c890233b61d74f33
sindyangraini <sindyangrraini214@gmail.com>
2019-02-18 12:36:55 +0700

    vid 19

--- resources/views/admin/pages/user/daftar.blade.php
+++ resources/views/admin/pages/user/daftar.blade.php
@@ -22,6 +22,24 @@
 </div>
 @endif
 
+@if(session('result')== 'delete')
+<div class="alert alert-succes alert-dismissible fade show">
+	<strong>Deleted!</strong>Berhasil dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
+@if(session('result')== 'fail-delete')
+<div class="alert alert-danger alert-dismissible fade show">
+	<strong>Failde!</strong>Gagal dihapus.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;
+	</button>
+</div>
+@endif
+
 
 <div class="row">
 	<div class="col-md-6 mb-3">
@@ -95,7 +113,7 @@
 
 			<div class="modal-body">
 				Apakah anda yakin ingin menghapusnya?
-				<form id="form-delete" method="post" action="#">
+				<form id="form-delete" method="post" action="{{ route('admin.user') }}">
 					{{ csrf_field() }}
 					{{ method_field('delete') }}
 					<input type="hidden" name="id" id="input-id">
@@ -122,7 +140,7 @@ $(function(){
 	});
 
 	$('.btn-delete').click(function(){
-		alert( $('#input-id').val() );
+		$('#form-delete').submit();
 	});
 })
 </script>
